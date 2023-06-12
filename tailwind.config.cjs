e24b96de63d00a44d87cc42d93d2317c72adf889
Strocs <strocsdev@gmail.com>
2023-06-12 18:04:09 -0400

    First project structure

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'light-green': '#d2f4d5',
+				'pale-green': '#5a7869',
+				lilac: '#8f87a8',
+			},
+		},
+	},
+	plugins: [],
+}
