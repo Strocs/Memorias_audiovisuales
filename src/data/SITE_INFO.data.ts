3366f70760abc1f86866185bbc757a10a0068e96
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-09 12:56:01 -0300

    add: site info

---
+++ src/data/SITE_INFO.data.ts
@@ -0,0 +1,7 @@
+export const SITE_INFO = {
+  title: 'Memoria Audiovisual',
+  subtitle: 'escuela de arquitectura | uls',
+  description: 'Entrevistas a titulados de Arquitectura de la Universidad de La Serena, en donde relatan lo que fue su proceso de titulación y cómo lograron entender a su manera lo que es arquitectura.',
+  OG_image: '/OG_image.png',
+  site_url: 'https://memoriaaudiovisual.cl/'
+}
