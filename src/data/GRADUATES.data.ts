f6f4ad7bd4563cdeea77023cdf2ff5afee51cf89
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-09 12:56:27 -0300

    change: data of graduates

---
+++ src/data/GRADUATES.data.ts
@@ -0,0 +1,80 @@
+// year, description, images, videos are fake info
+
+import type { Graduates } from '../types/graduates'
+
+export const GRADUATES: Graduates = [
+	{
+		name: 'Diego Alvarado',
+		full_name: 'Diego Alvarado',
+		project_name:
+			'La Cotidianidad de la Tecnologia y la Realidad Virtual en un Mundo Entre Pandemias',
+		project_year: '2022',
+		project_description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae saepe modi necessitatibus qui, sit, hic beatae nobis non perspiciatis minima doloremque consequuntur laborum cum tempore numquam fugit ducimus cumque at suscipit odit ratione, eos totam adipisci ipsa. Voluptatibus officiis a voluptates inventore earum deleniti magni possimus. Doloribus labore pariatur minus at quam quod architecto unde. Ut officiis nobis delectus facere, et voluptates autem rem possimus mollitia, sed cum corporis similique ab corrupti modi atque adipisci iusto dignissimos a quaerat voluptate? Impedit blanditiis iusto quam doloremque, excepturi, eius, aliquam corporis ducimus sapiente tenetur sit? Magni facere illo laboriosam fugiat animi, rem impedit quia nemo quaerat dolor perspiciatis amet mollitia harum maxime consectetur praesentium ratione earum iste commodi distinctio enim quae. Cum iste doloribus dolore! Laboriosam recusandae inventore a atque nulla enim harum tempora? Qui incidunt soluta officia tempore explicabo debitis cum ullam aut repellendus, molestias sit deserunt tenetur, blanditiis dolorem atque.',
+		reel_id: 'mock1',
+		video_id: '8yMzZwAtZRw',
+		project_images: [
+			'https://images.adsttc.com/media/images/649e/c937/cb9c/465d/b291/5cb2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_7.jpg?1688127837',
+			'https://images.adsttc.com/media/images/649e/c7cd/cb9c/465d/b291/5ca5/slideshow/m2-2.jpg?1688127459',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_15.jpg?1688127961',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d1/slideshow/workers-pavilion-no-architects-designers-and-social-artists_13.jpg?1688127953',
+			'https://images.adsttc.com/media/images/649e/c88c/cb9c/465d/b291/5caa/slideshow/m1-1.jpg?1688127652',
+			'https://images.adsttc.com/media/images/649e/c7bd/5921/186c/68fb/16c8/slideshow/m5-5.jpg?1688127447'
+		],
+	},
+	{
+		name: 'Cristían Navarro',
+		full_name: 'Cristian Navarro Mondaca',
+		project_name: "Albergue Oncologico 'El Faro y La Gruta'",
+		project_year: '2023',
+		project_description:
+			'El alberge oncologico pretende entregar una atencion holistica a todos los pacientes con cancer d ela región. La mediicina holistica se entiende como el enfoque amplio ante el tratamiento de una enfermedad y no unicamente a traves de la medicina convencional. En el cancer el acompañamineto del paciente a traves de su proceso es crucial para un tratamiento con mas posibilidades de exito, y tambien para mantener su calidad de vida a pesar y junto al tratamiento tradicional, que es por lo general muy invasivo y desgastante. \n La obra de arquitectura se pone al servicio de la visibilización del cancer para todo aquel que tenga la posibilidad de apreciarla en su caracter nocturno (se encuentra en una zona de granvisivilidad urbana). En el volumen vertical se plasmaran los colores en la fecha determinada que presentan a cada tipo de cancer segun la OMS. La curiosidad por este hecho pretende desencadenar una simple busqueda en internet, busqueda que se piede vincular a una determinada pagina web donde se explica el porque del hecho y se introducira en la importancia de la concientización acerca del cancer.',
+		reel_id: 'mock2',
+		video_id: '8yMzZwAtZRw',
+		project_images: [
+			'https://images.adsttc.com/media/images/649e/c937/cb9c/465d/b291/5cb2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_7.jpg?1688127837',
+			'https://images.adsttc.com/media/images/649e/c7cd/cb9c/465d/b291/5ca5/slideshow/m2-2.jpg?1688127459',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_15.jpg?1688127961',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d1/slideshow/workers-pavilion-no-architects-designers-and-social-artists_13.jpg?1688127953',
+			'https://images.adsttc.com/media/images/649e/c88c/cb9c/465d/b291/5caa/slideshow/m1-1.jpg?1688127652',
+			'https://images.adsttc.com/media/images/649e/c7bd/5921/186c/68fb/16c8/slideshow/m5-5.jpg?1688127447'
+		],
+	},
+	{
+		name: 'María Paulina Godoy',
+		full_name: 'María Paulina Godoy Alvarez',
+		project_name:
+			"Humedal El Culebron 'Dispositivos Arquitectonicos para su Valorización, Preservación y Visualización'",
+		project_year: '2021',
+		project_description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae saepe modi necessitatibus qui, sit, hic beatae nobis non perspiciatis minima doloremque consequuntur laborum cum tempore numquam fugit ducimus cumque at suscipit odit ratione, eos totam adipisci ipsa. Voluptatibus officiis a voluptates inventore earum deleniti magni possimus. Doloribus labore pariatur minus at quam quod architecto unde. Ut officiis nobis delectus facere, et voluptates autem rem possimus mollitia, sed cum corporis similique ab corrupti modi atque adipisci iusto dignissimos a quaerat voluptate? Impedit blanditiis iusto quam doloremque, excepturi, eius, aliquam corporis ducimus sapiente tenetur sit? Magni facere illo laboriosam fugiat animi, rem impedit quia nemo quaerat dolor perspiciatis amet mollitia harum maxime consectetur praesentium ratione earum iste commodi distinctio enim quae. Cum iste doloribus dolore! Laboriosam recusandae inventore a atque nulla enim harum tempora? Qui incidunt soluta officia tempore explicabo debitis cum ullam aut repellendus, molestias sit deserunt tenetur, blanditiis dolorem atque.',
+		reel_id: 'mock3',
+		video_id: '8yMzZwAtZRw',
+		project_images: [
+			'https://images.adsttc.com/media/images/649e/c937/cb9c/465d/b291/5cb2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_7.jpg?1688127837',
+			'https://images.adsttc.com/media/images/649e/c7cd/cb9c/465d/b291/5ca5/slideshow/m2-2.jpg?1688127459',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_15.jpg?1688127961',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d1/slideshow/workers-pavilion-no-architects-designers-and-social-artists_13.jpg?1688127953',
+			'https://images.adsttc.com/media/images/649e/c88c/cb9c/465d/b291/5caa/slideshow/m1-1.jpg?1688127652',
+			'https://images.adsttc.com/media/images/649e/c7bd/5921/186c/68fb/16c8/slideshow/m5-5.jpg?1688127447'
+		],
+	},
+	{
+		name: 'Cristián Perez',
+		full_name: 'Cristián Perez Vásquez',
+		project_name: 'Museo Regional de la Memoria ',
+		project_year: '2021',
+		project_description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae saepe modi necessitatibus qui, sit, hic beatae nobis non perspiciatis minima doloremque consequuntur laborum cum tempore numquam fugit ducimus cumque at suscipit odit ratione, eos totam adipisci ipsa. Voluptatibus officiis a voluptates inventore earum deleniti magni possimus. Doloribus labore pariatur minus at quam quod architecto unde. Ut officiis nobis delectus facere, et voluptates autem rem possimus mollitia, sed cum corporis similique ab corrupti modi atque adipisci iusto dignissimos a quaerat voluptate? Impedit blanditiis iusto quam doloremque, excepturi, eius, aliquam corporis ducimus sapiente tenetur sit? Magni facere illo laboriosam fugiat animi, rem impedit quia nemo quaerat dolor perspiciatis amet mollitia harum maxime consectetur praesentium ratione earum iste commodi distinctio enim quae. Cum iste doloribus dolore! Laboriosam recusandae inventore a atque nulla enim harum tempora? Qui incidunt soluta officia tempore explicabo debitis cum ullam aut repellendus, molestias sit deserunt tenetur, blanditiis dolorem atque.',
+		reel_id: 'mock4',
+		video_id: '8yMzZwAtZRw',
+		project_images: [
+			'https://images.adsttc.com/media/images/649e/c937/cb9c/465d/b291/5cb2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_7.jpg?1688127837',
+			'https://images.adsttc.com/media/images/649e/c7cd/cb9c/465d/b291/5ca5/slideshow/m2-2.jpg?1688127459',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d2/slideshow/workers-pavilion-no-architects-designers-and-social-artists_15.jpg?1688127961',
+			'https://images.adsttc.com/media/images/649e/c9ad/5921/186c/68fb/16d1/slideshow/workers-pavilion-no-architects-designers-and-social-artists_13.jpg?1688127953',
+			'https://images.adsttc.com/media/images/649e/c88c/cb9c/465d/b291/5caa/slideshow/m1-1.jpg?1688127652',
+			'https://images.adsttc.com/media/images/649e/c7bd/5921/186c/68fb/16c8/slideshow/m5-5.jpg?1688127447'
+		],
+	}
+]
---
+++ src/types/graduates.d.ts
@@ -0,0 +1,13 @@
+export interface Graduate {
+  name: string;
+  full_name: string
+  project_name: string;
+  project_year: string;
+  project_description: string;
+  project_images: string[];
+  reel_id: string;
+  video_id: string;
+}
+
+export type Graduates = Graduate[]
+
