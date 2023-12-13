9be7580b0893092267a0ce439ce6934f1e0d65fa
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-13 12:19:30 -0300

    fix: swiper up error

--- src/data/SITE_INFO.ts
+++ src/data/SITE_INFO.ts
@@ -3,5 +3,5 @@ export const SITE_INFO = {
   subtitle: 'escuela de arquitectura | uls',
   description: 'Memoria Audiovisual busca ser una ventana de difusión para trabajos de titulación con la comunidad dentro y fuera de la universidad, cómo también un material de apoyo para estudiantes que comienzan su proceso y ver cómo otros sobrepasaron las barreras del proceso.',
   OG_image: '/OG_image.png',
-  site_url: 'https://memoriaaudiovisual.cl/'
+  site_url: 'https://memoriauls.cl/'
 }
