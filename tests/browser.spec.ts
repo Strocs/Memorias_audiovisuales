fb6b35fcafcb151ccc30a6a07809329f3eda6870
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-15 14:51:39 -0300

    add: browser tests

---
+++ tests/browser.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+const LOCAL_URL = 'http://localhost:4321'
+const URL = 'https://memorias-audiovisuales.vercel.app/'
+
+test('has title', async ({ page }) => {
+  await page.goto(URL);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Memoria Audiovisual/);
+});
