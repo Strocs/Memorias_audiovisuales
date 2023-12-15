ee3f64a67c0009a7d0bc61bf2fa7d955efe89048
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-15 16:49:51 -0300

    chore: remove react from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
 import vercel from '@astrojs/vercel/static'
 
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react(), sitemap()],
+	integrations: [tailwind(), sitemap()],
 	site: 'https://memorias-audiovisuales.vercel.app',
 	output: 'static',
 	adapter: vercel({
