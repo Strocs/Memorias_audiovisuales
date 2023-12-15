92fd198d97635861f1dbee42ad1a31d00383e6f6
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-15 16:24:03 -0300

    add: sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-
 import vercel from '@astrojs/vercel/static'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()],
+	integrations: [tailwind(), react(), sitemap()],
+	site: 'https://memorias-audiovisuales.vercel.app',
 	output: 'static',
 	adapter: vercel({
 		webAnalytics: {
