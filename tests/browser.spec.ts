1dbc6f99cecd761256d8a86810400bed2f7fa5f8
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-15 15:11:40 -0300

    fix: aspect ratio on video reels

--- tests/browser.spec.ts
+++ tests/browser.spec.ts
@@ -4,7 +4,7 @@ const LOCAL_URL = 'http://localhost:4321'
 const URL = 'https://memorias-audiovisuales.vercel.app/'
 
 test('has title', async ({ page }) => {
-  await page.goto(URL);
+  await page.goto(LOCAL_URL);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Memoria Audiovisual/);
